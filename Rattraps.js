b9ee885990e41628f517616c88f98020a65a61d4
warmest-soup <billyjackburner2@gmail.com>
2024-10-27 03:27:43 -0700

    Update Rattraps.js

--- Rattraps.js
+++ Rattraps.js
@@ -1,6 +1,26 @@
 console.log(document.getElementById("skl1"));
+
+$(document).ready(function(){
+  
+  let hovercount = 1;
+  $(".sklSlot").hover(
+    
+    function selectAudio(event){
+      console.log("Running");
+      hovercount++;
+      if(hovercount%2==0){
+      var select = new Audio('https://github.com/warmest-soup/warmest-soup.github.io/raw/refs/heads/main/Assets/Audio/AbilitySelect.mp3');
+      select.play();
+      console.log(hovercount);
+      
+    };
+  });
+});
+
+
 function dragStart(event) {
   event.dataTransfer.setData("Text", event.target.id);
+  
   //console.log(document.getElementById(event.target.id)); 
 }
 function allowDrop(event) {
@@ -11,6 +31,8 @@ function allowDrop(event) {
 function drop(event) {
   event.preventDefault();
   var data = event.dataTransfer.getData("Text");
-  event.target.appendChild(document.getElementById(data));
+  event.target.appendChild(document.getElementById(data)); 
   event.dataTransfer.clearData("Text");
+  var equip = new Audio('https://github.com/warmest-soup/warmest-soup.github.io/raw/refs/heads/main/Assets/Audio/AbilityDrop.mp3');
+  equip.play();
 }
