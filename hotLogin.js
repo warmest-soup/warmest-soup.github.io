0f7d13d33bfa92a226eb4c40e5cc3416df664d7e
warmest-soup <billyjackburner2@gmail.com>
2025-10-08 05:09:51 -0500

    Update hotLogin.js

--- hotLogin.js
+++ hotLogin.js
@@ -1,1 +1,11 @@
-console.log("HotLog Running");
+
+var urldata=new URLSearchParams(window.location.search);
+
+var charSheet=document.getElementById("GAScontent");
+var sheetSource=charSheet.getAttribute("src");
+
+if(urldata.get("hlgU")&&urldata.get("hlgK")){
+  charSheet.setAttribute("src",
+    sheetSource + "?hlgU=" + urldata.get("hlgU") + "&hlgK=" + urldata.get("hlgK")
+  );
+}
