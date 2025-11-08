51ee162eb1f46e66783dd93c4a8cdd5fdbc28e9d
warm-soup <billyjackburner2@gmail.com>
2025-11-08 04:12:00 -0600

    aeh

--- RATTRAPS/DocReaderScript.js
+++ RATTRAPS/DocReaderScript.js
@@ -14,15 +14,15 @@
 }
 
 /*Format all key terms*/{
-    var mainText=document.getElementById("mainText");
-    var mainFormat=mainText.innerHTML;
-     
+    var mainText = document.getElementById("mainText");
+    var mainFormat = mainText.innerHTML;
+
     //Word exceptions
-    var wordSearches=Array.from(keyTerms);
-    function updateTerm(id, func){
-        wordSearches[keyTerms.indexOf(id)]=func;
+    var wordSearches = Array.from(keyTerms);
+    function updateTerm(id, func) {
+        wordSearches[keyTerms.indexOf(id)] = func;
     }
-    
+
     updateTerm("Direction", "Direct");
     updateTerm("Char-Sheet", "Sheet");
     updateTerm("Checks", "Check");
@@ -35,41 +35,58 @@
     updateTerm("Retirement", "Retire");
     updateTerm("Penalty", "Penalt");
     updateTerm("Incumbents", "Incumbent");
-    
-    wordSearches.forEach((x)=>{
-        var l1=x.substring(0,1);
-        var termIndex=wordSearches.indexOf(x)+1;
-        var xl=x.substring(1); 
-        
-        var replacement=
-        "<a style='display:inline-block;' href=#"+keyTerms[termIndex-1]+"><i>$3"+xl+"$4<sup>"+termIndex+"</sup></i></a>";
-         
+    updateTerm("Classes", "Class");
+    updateTerm("Bonuses", "Bonus");
+    updateTerm("Techniques", "Technique")
+
+
+    wordSearches.forEach((x) => {
+        var l1 = x.substring(0, 1);
+        var termIndex = wordSearches.indexOf(x) + 1;
+        var xl = x.substring(1);
+
+        var replacement =
+            "<a style='display:inline-block;' href=#" + keyTerms[termIndex - 1] + "><i>$3" + xl + "$4<sup>" + termIndex + "</sup></i></a>";
+
         var pattern = new RegExp(
-           `((?<!(\"|>|-))(${l1}|${l1.toLowerCase()})${xl}(y|ies|sets|s|'s|es|ed|ment|ion|ing)?)(?!:|s:)`,"g" 
+            `((?<!(\"|>|-))(${l1}|${l1.toLowerCase()})${xl}(y|ies|sets|s|'s|es|ed|ment|ion|ing)?)(?!:|s:|=")`, "g"
         );
-        
-        
-        mainFormat=mainFormat.replace(
-          pattern ,
-          replacement
+
+
+        mainFormat = mainFormat.replace(
+            pattern,
+            replacement
         );
     });
-    mainText.innerHTML=mainFormat;
+    mainText.innerHTML = mainFormat;
 }
 
 /*Format terms entries*/{
     var entries = Array.from(document.getElementsByClassName("Term"));
-    entries.forEach((x)=>{
-        if(x.parentNode.parentNode.id=="definitions"){
-            x.innerText=(keyTerms.indexOf(x.id)+1)+". "+x.innerHTML;
+    entries.forEach((x) => {
+        if (x.parentNode.parentNode.id == "definitions") {
+            x.innerText = (keyTerms.indexOf(x.id) + 1) + ". " + x.innerHTML;
         }
-        
+
     });
 }
 
 /*Exectional Exceptions like Deaths/Dies*/{
-    mainText.innerHTML=mainText.innerHTML.replace( 
-        /(?<!>)([Dd]ie|[Dd]eath)(s?)/g,"<a style='display:inline-block;' href='#Death'><i>$1$2<sup>"+keyTerms.indexOf("Death")+"</sup></i></a>");
+    //Death
+    mainText.innerHTML = mainText.innerHTML.replace(
+        /(?<!>)([Dd]ie|[Dd]eath)(s?)/g, "<a style='display:inline-block;' href='#Death'><i>$1$2<sup>" + keyTerms.indexOf("Death") + "</sup></i></a>");
+
+    //DP
+    updateTerm("Death-Points", "DP");
+    mainText.innerHTML = mainText.innerHTML.replace(
+        /(?<!>)DP(?!<)/g, "<a style='display:inline-block;' href='#Death'><i>DP<sup>" + keyTerms.indexOf("Death-Points") + "</sup></i></a>");
+
+}
+
+/*UrlHighlight*/{
+    if(window.location.hash){
+        console.log("Redirected!!!!");
+    }
 }
 
 
