1fb6b0febdda0ce35f9b085302ca30ae644c04ed
warm-soup <billyjackburner2@gmail.com>
2025-12-01 10:30:08 -0600

    fd

--- RATTRAPS/DocReaderScript.js
+++ RATTRAPS/DocReaderScript.js
@@ -75,7 +75,7 @@
             "<a style='display:inline-block;' href=#" + keyTerms[termIndex - 1] + "><i>$3" + xl + "$4<sup>" + termIndex + "</sup></i></a>";
 
         var pattern = new RegExp(
-            `((?<!(\"|>|-|\\w))(${l1}|${l1.toLowerCase()})${xl}(y|ies|sets|s|'s|y's|es|ed|e|ful|ment|ity|led|ion|ing|al|d)?)(?!:|s:|ment:|=")`, "g"
+            `((?<!(\"|>|-|\\w))(${l1}|${l1.toLowerCase()})${xl}(y|ies|sets|s|'s|y's|es|ed|e|ful|ment|ity|led|ion|ing|al|d)?)(?!:|s:|or|ment:|=")`, "g"
         );  
 
 
